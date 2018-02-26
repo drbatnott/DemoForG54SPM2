ca04400e1efd58617db058ac44be84ae17f150ea
Peter Blanchfield <pxb@cs.nott.ac.uk>
2018-02-26 15:14:36 +0000

    Refactored Block Select Code
    changed the way it was being done so saved some time in refactoring

--- MinecraftRevisited/Assets/TestSpawnBlock.cs
+++ MinecraftRevisited/Assets/TestSpawnBlock.cs
@@ -4,18 +4,23 @@ using System.Collections;
 public class TestSpawnBlock : MonoBehaviour {
 	public Vector3 startPos;
 	public GameObject [] spawnObjects;
+	public int obNumber;
 	// Use this for initialization
 	void Start () {
 		startPos = new Vector3 (-8f, -2f, 0f);
+		obNumber = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Alpha1)){
-			GameObject spawned = GameObject.Instantiate (spawnObjects[0]);
+		if(Input.GetKeyDown (KeyCode.Space)){
+			GameObject spawned = GameObject.Instantiate (spawnObjects[obNumber]);
 			spawned.transform.position = startPos;
 			startPos.x +=2;
 		}
 	
+	}
+	public void SetOb(int i){
+		obNumber = i;
 	}
 }
