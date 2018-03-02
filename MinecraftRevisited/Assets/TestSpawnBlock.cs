ffbaaea8f4ecc808f7fafa7c5095c6447c06338a
Peter Blanchfield <pxb@cs.nott.ac.uk>
2018-03-02 12:36:59 +0000

    Integrated FPC with makeblocks
    violated some earlier tests so had to refactor

--- MinecraftRevisited/Assets/TestSpawnBlock.cs
+++ MinecraftRevisited/Assets/TestSpawnBlock.cs
@@ -5,18 +5,44 @@ public class TestSpawnBlock : MonoBehaviour {
 	public Vector3 startPos;
 	public GameObject [] spawnObjects;
 	public int obNumber;
+	public GameObject hammer;
+	Transform t;
+	Vector3 displacement;
+	bool spawning = false;
+	bool rotating = false;
+	int count;
 	// Use this for initialization
 	void Start () {
-		startPos = new Vector3 (-8f, -2f, 0f);
+		t = hammer.GetComponent<Transform> ();
+		//startPos = t.position +displacement ;
 		obNumber = 0;
+		count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space)){
+
+		if(Input.GetKeyDown (KeyCode.Space) && !spawning){
+			spawning =true;
+			rotating = true;
+			count = 0;
+		}
+		if (!rotating && spawning){
 			GameObject spawned = GameObject.Instantiate (spawnObjects[obNumber]);
+			displacement = 2 * t.forward + new Vector3 (0f, -2f, 0f);
+			startPos = t.position + displacement;
 			spawned.transform.position = startPos;
-			startPos.x +=2;
+			spawning = false;
+
+
+		}
+		if (spawning && rotating) {
+			t.RotateAround (Vector3.zero,Vector3.left,10f);
+			count++;
+			if(count == 10){
+				rotating = false;
+				t.RotateAround (Vector3.zero,Vector3.left,-100f);
+			}
 		}
 	
 	}
